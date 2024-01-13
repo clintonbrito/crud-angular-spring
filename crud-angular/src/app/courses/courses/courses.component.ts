c13fd7583d11551246e3a28f82272ae4fca1c350
Bill <clintoncsb@gmail.com>
2024-01-12 21:33:14 -0300

    ✨ feat: edit `courses` component to be a mat-table

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+// import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
+  courses: Course[] = [
+    { _id: '1', name: 'Angular', category: 'Front-end' }
+  ];
+  displayedColumns = ['name', 'category'];
+
+  constructor() {
+    // this.courses = []; // É possível inicializar o array aqui ou na linha acima onde foi declarado.
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
