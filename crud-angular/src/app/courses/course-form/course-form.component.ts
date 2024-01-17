704cd96b31eea21662feb3a055f6894ba4047e09
Bill <clintoncsb@gmail.com>
2024-01-17 17:53:34 -0300

    ✨ feat: create new course form

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-course-form',
   templateUrl: './course-form.component.html',
   styleUrl: './course-form.component.scss'
 })
-export class CourseFormComponent {
+export class CourseFormComponent implements OnInit {
+
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      name : [null],
+      category: [null]
+    })
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  onSubmit() {
+    console.log('onSubmit');
+  }
+
+  onCancel() {
+    console.log('onCancel');
+  }
 
 }
