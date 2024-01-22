d70faa11598c6d3936706defe949068d7fdf17a7
Bill <clintoncsb@gmail.com>
2024-01-21 23:25:14 -0300

    ✨ feat: save updated form after update a course

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -25,7 +25,26 @@ export class CoursesService {
     return this.httpClient.get<Course>(`${this.API}/${id}`);
   }
 
-  save(record: Partial<Course>) {
-    return this.httpClient.post<Course>(this.API, record).pipe(first());
+  save(data: Partial<Course>) {
+    // console.log('primeiro console: ', data);
+
+    // data._id ? this.update(data) : this.create(data);
+
+    if (data._id) {
+      // console.log('updated: ', data);
+      return this.update(data);
+    }
+
+    // console.log('created: ', data);
+    return this.create(data);
+  }
+
+  private create(data: Partial<Course>) {
+    return this.httpClient.post<Course>(this.API, data).pipe(first());
+  }
+
+  private update(data: Partial<Course>) {
+    return this.httpClient.put<Course>(`${this.API}/${data._id}`, data).pipe(first());
+
   }
 }
