44c9957b6da74aea1cc492e8f3ece9a4ded8ee3d
Bill <clintoncsb@gmail.com>
2024-02-26 19:03:36 -0300

    ✨ add Soft Delete to db using Hibernate Annotations

--- crud-spring/src/main/java/com/clintonbrito/crudspring/model/CourseModel.java
+++ crud-spring/src/main/java/com/clintonbrito/crudspring/model/CourseModel.java
@@ -12,10 +12,15 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 @Data // está incluso os getters e setters do Lombok
 @Entity // Estou dizendo que a classe CourseModel também será uma entidade que vai fazer o mapeamento no db
+@SQLDelete(sql = "UPDATE courses SET status = 'Inactive' WHERE id = ?")
+@SQLRestriction("status <> 'Inactive'")
 @Table(name = "courses")
 public class CourseModel {
 
@@ -35,5 +40,11 @@ public class CourseModel {
   @Pattern(regexp = "Back-end|Front-end")
   @Column(name = "categoria", length = 10, nullable = false)
   private String category;
+
+  @NotNull
+  @Length(max = 10)
+  @Pattern(regexp = "Active|Inactive")
+  @Column(length = 10, nullable = false)
+  private String status = "Active";
 
 }
