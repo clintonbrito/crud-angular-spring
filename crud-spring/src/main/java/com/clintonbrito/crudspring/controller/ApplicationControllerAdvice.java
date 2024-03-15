30491ebc772f4f356d509b25c3fb9a34245a6845
Bill <clintoncsb@gmail.com>
2024-03-15 19:25:49 -0300

    🥅 feat: handle API's validation errors

--- crud-spring/src/main/java/com/clintonbrito/crudspring/controller/ApplicationControllerAdvice.java
+++ crud-spring/src/main/java/com/clintonbrito/crudspring/controller/ApplicationControllerAdvice.java
@@ -1,12 +1,14 @@
 package com.clintonbrito.crudspring.controller;
 
 import com.clintonbrito.crudspring.exception.RecordNotFoundException;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
@@ -24,6 +26,28 @@ public class ApplicationControllerAdvice {
                 .map(error -> ((FieldError) error).getField() + " " + error.getDefaultMessage())
 //                .reduce("", (acc, error) -> acc + error + "\n");
                 .toList().toString();
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleConstraintViolationException(ConstraintViolationException ex) {
+        return ex.getConstraintViolations().stream()
+                .map(violation -> violation.getPropertyPath() + " " + violation.getMessage())
+                .toList().toString();
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
+        if (ex != null && ex.getRequiredType() != null) {
+            String type = ex.getRequiredType().getName();
+            // java.lang.Long
+            String[] typeParts = type.split("\\.");
+            // Long
+            String typeName = typeParts[typeParts.length - 1];
+            return ex.getName() + " should be of type " + typeName;
+        }
+        return "Invalid argument type";
     }
 
 
