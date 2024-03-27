ef3da6225ee7b1014d262978d509dd4d301ac7e0
Clinton Brito <clintoncsb@gmail.com>
2024-03-27 20:11:04 -0300

    ⬆️ feat: bootstrap the application using standalone APIs

--- crud-angular/src/main.ts
+++ crud-angular/src/main.ts
@@ -1,7 +1,23 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app/app-routing.module';
+import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
+import { provideClientHydration, BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule, MatToolbarModule),
+        provideClientHydration(),
+        provideHttpClient(withFetch()) // Inseri essa linha aqui para tratar o warning #NG02801 que aparecia no console o tempo todo
+        ,
+        provideAnimations(),
+        provideHttpClient(withInterceptorsFromDi())
+    ]
+})
   .catch(err => console.error(err));
