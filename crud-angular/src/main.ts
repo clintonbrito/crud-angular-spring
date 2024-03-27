4e50f960c03a7637c4bc08374725d173826657b6
Clinton Brito <clintoncsb@gmail.com>
2024-03-27 20:33:40 -0300

    ♻️ refactor: update routing files

--- crud-angular/src/main.ts
+++ crud-angular/src/main.ts
@@ -1,23 +1,24 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
-import { importProvidersFrom } from '@angular/core';
-import { AppComponent } from './app/app.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app/app-routing.module';
 import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
-import { provideClientHydration, BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { importProvidersFrom } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { bootstrapApplication, BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
+
+import { AppComponent } from './app/app.component';
+import { APP_ROUTES } from './app/app.routes';
+
 
 
 bootstrapApplication(AppComponent, {
     providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule, MatToolbarModule),
+        importProvidersFrom(BrowserModule, MatToolbarModule),
         provideClientHydration(),
         provideHttpClient(withFetch()) // Inseri essa linha aqui para tratar o warning #NG02801 que aparecia no console o tempo todo
         ,
         provideAnimations(),
-        provideHttpClient(withInterceptorsFromDi())
+        provideHttpClient(withInterceptorsFromDi()),
+        provideRouter(APP_ROUTES, withPreloading(PreloadAllModules))
     ]
 })
   .catch(err => console.error(err));
